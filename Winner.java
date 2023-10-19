86ee20b3be320ea0d08277ebcab9c29d929c1906
Isabella Wang <wangisa26@7hills.org>
2023-10-19 15:23:57 -0400

    comments winner

--- Winner.java
+++ Winner.java
@@ -4,7 +4,7 @@ public class Winner {
     int rowNums, colNums, maxTries, guessRow, guessCol;
     int tryNum = 1; // to adjust for human counting
 
-    public Winner (Board play1, Board play2, int retries)
+    public Winner (Board play1, Board play2, int retries) //constructor
     {
         answers = play1;
         guesses = play2;
@@ -13,26 +13,11 @@ public class Winner {
         colNums = answers.grid[0].length;
     }
 
-    public boolean chickendinner () // check win condition
-    {   
-        for (int i = 0; i < rowNums; i++)
-        {
-            for (int j = 0; j < colNums; j++)
-            {
-                if (answers.grid[i][j] != 'O' && guesses.grid[i][j] == 'O') // a ship has not been guessed yet
-                    return false;
-            }
-        } // if this return false and max tries exceeded, player 1/computer won.
-        
-        return true;
-        
-    }
-
     public boolean match( int rowG, int colG) // check if hit
     {
         tryNum++;
-        guessRow = rowG;
-        guessCol = colG;
+        guessRow = rowG; //i originall didn't do this but then i would end up having to pass the inputs back  and forth between functions inside here so. yea. 
+        guessCol = colG;//not worth it and too much of a headache to keep straight.
         
         if (tryNum > maxTries)
         {
@@ -43,28 +28,30 @@ public class Winner {
             return true;
         }
         else if (tryNum == maxTries)
-            System.out.println("Final shot!");
+            System.out.println("Final shot!");//hehe pun
         else
             System.out.println("Guess #" + tryNum);
 
         if (guesses.grid[guessRow][colG] != 'O')
         {
             System.out.println("You already guessed that spot! Try again. ");
-            tryNum--;
+            tryNum--; //gives them their try back
             guesses.printBoard();
             return false;
         }
 
         if (answers.grid[guessRow][guessCol] != 'O')
         {
-            guesses.grid[guessRow][guessCol] = '!'; // backslash has value 92 and i'm not dealing w/ the headache of escape codes
+            guesses.grid[guessRow][guessCol] = '!'; // originally. i set the ends of ships to a different sunk. but that's not very fair. so boo :(
             
-            if (sunk() == 0)
-                System.out.println("Sunk!");
-            else if (sunk() == 2)
+            if (sunk())
             {
-
-                return true;
+                if (chickendinner())
+                {
+                   System.out.println("Player 2 wins!");
+                   return true;//ends it early since p2 won
+                }
+                System.out.println("Sunk!"); // since it returns no need for else part
             }
             else
             {
@@ -77,44 +64,42 @@ public class Winner {
             guesses.grid[guessRow][guessCol] = '.';
             System.out.println("Miss!");
         }
-        guesses.printBoard();
+        guesses.printBoard(); //let's person see the board and help them decide where to judge next
         return false;
     }
 
-    public int sunk() // check if sunk.. all ships must be sunk for player 1 to win so check here to lessen running time. return 0 for sunk, 1 for false and 2 for winner
+    public boolean sunk() // check if sunk.. all ships must be sunk for player 1 to win so check here to lessen running time. return 0 for sunk, 1 for false and 2 for winner
     {
         int cursor,start; //cursor should be at left top poitn (\) then continue to check down to  /
-        
-        if (chickendinner())
+
+        //og i tested chickendinner here before realizing...wait i can test it inside the hit
+
+        if (reOrient())
         {
-            return 2;
-        }
-        else
-        {
-            if (reOrient())
+            cursor = guessCol;//remember for horizontal, the columns are the ones changing
+            while (cursor >= 1 && answers.grid[guessRow][cursor - 1] != '<') //scroll all the way to the start
+            {    
+                cursor--; 
+            }
+
+        start = cursor;
+
+            while (cursor < colNums)
             {
-                cursor = guessCol;//remember for horizontal, the columns are the ones changing
-                while (cursor >= 1 && answers.grid[guessRow][cursor - 1] != '<') //scroll all the way to the start
-                {    cursor--; }
-
-                start = cursor;
-
-                while (cursor < colNums)
+                if (answers.grid[guessRow][cursor] != 'O' && guesses.grid[guessRow][cursor] == 'O')
+                    return false;
+                else if (answers.grid[guessRow][cursor] == '>' && guesses.grid[guessRow][cursor] == '!')
                 {
-                    if (answers.grid[guessRow][cursor] != 'O' && guesses.grid[guessRow][cursor] == 'O')
-                        return 1;
-                    else if (answers.grid[guessRow][cursor] == '>' && guesses.grid[guessRow][cursor] == '!')
+                    do
                     {
-                        do
-                        {
-                            guesses.grid[guessRow][start] = 'X';
-                            start++;
-                        }
-                        while (answers.grid[guessRow][start] != 'O');
-                        return 0;
+                        guesses.grid[guessRow][start] = 'X';
+                         start++;
                     }
-                    cursor++;
+                    while (answers.grid[guessRow][start] != 'O');
+                    return true;
                 }
+                 cursor++;
+               }
             }
             else
             {
@@ -128,7 +113,7 @@ public class Winner {
                 while (cursor < rowNums)
                 {
                     if (answers.grid[cursor][guessCol] != 'O' && guesses.grid[cursor][guessCol] == 'O')
-                        return 1;
+                        return false;
                     else if (answers.grid[cursor][guessCol] == '>' && guesses.grid[cursor][guessCol] == '!')
                     {
                         do
@@ -137,13 +122,12 @@ public class Winner {
                             start++;
                         }
                         while (answers.grid[guessRow][start] != 'O');
-                        return 0;
+                        return true;
                     }
                     cursor++;
                 }
             }
-        }
-        return 1;
+        return false;
     }
 
     public boolean reOrient ()//return true for horizontal, false for vertical
@@ -160,5 +144,19 @@ public class Winner {
         }
 
         return false;
+    }
+
+    public boolean chickendinner () // check win condition
+    {   //also i moved this to the end bc. thats where ud expect the win condition right??SS
+        for (int i = 0; i < rowNums; i++)
+        {
+            for (int j = 0; j < colNums; j++)
+            {
+                if (answers.grid[i][j] != 'O' && guesses.grid[i][j] == 'O') // a ship has not been guessed yet
+                    return false;
+            }
+        } // if this return false and max tries exceeded, player 1/computer won.
+        
+        return true; 
     }
 }
