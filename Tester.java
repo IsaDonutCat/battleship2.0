4f14a4123821e019cbcf8a6b0ca72531dcb56af3
IsaDonutCat <114428942+IsaDonutCat@users.noreply.github.com>
2023-10-18 21:28:24 -0400

    finished 1.0 of winner

--- Tester.java
+++ Tester.java
@@ -23,13 +23,21 @@ public class Tester
 
         if (answered.equals("fast"))
         {
+            System.out.println("Welcome to fast mode!");
+            System.out.println("In this mode, the computer will place 3 ships: a submarine (3 units long), a battleship (4 units long), and a carrier (5 units long).  ");
+            System.out.println("Then, you will have 20 guesses. An 'O' will represent an unknown location, \na '.' will represent a miss, a '!' will represent a hit and a 'x' will represent a sunk ship.");
+            System.out.println("If you manage to sink all the ships in the given number of guesses, you will win. Otherwise, the computer will win.");
             rows = 8;
             cols = 8;
-            shipSet = Arrays.copyOfRange(shipSet,0,3);
+            shipSet = Arrays.copyOfRange(shipSet,2,5);
             choice = false;
         }
         else
         {
+            System.out.println("Welcome to regular mode!");
+            System.out.println("In this mode, player 1 will place 5 ships: a PT Boat (2 units long), a submarine (3 units long),\na destroyer (3 units long), a battleship (4 units long), and a carrier (5 units long).  ");
+            System.out.println("Then, player 2 will have a given number of guesses. An 'O' will represent an unknown location,\na '.' will represent a miss, a '!' will represent a hit and a 'x' will represent a sunk ship.");
+            System.out.println("If player 2 manages to sink all the ships in the given number of guesses, player 2 will win. Otherwise, player 1 will win.");
             rows = 10;
             cols = 10;
             choice = true;
@@ -37,7 +45,18 @@ public class Tester
 
         Board answers = new Board(rows, cols, choice);
         answers.placePieces(shipSet);
+        Board guesses = new Board(row, cols, choice);
 
-        
+        if (choice)
+        {
+            Scanner mainInput2 = new Scanner(System.in);
+            System.out.print("# of guesses: ");
+            Winner referee = new Winner(answers, guesses, mainInput2.nextInt());
+            ans = mainInput2.nextLine(); //clears the buffer
+        }
+        else
+        {
+            Winner referee = new Winner(answers, guesses, 20);
+        }
     }   
 }
--- Winner.java
+++ Winner.java
@@ -9,6 +9,8 @@ public class Winner {
         answers = play1;
         guesses = play2;
         maxTries = retries;
+        rowNums = answers.length;
+        colNums = answers[0].length;
     }
 
     public boolean chickendinner () // check win condition
@@ -26,7 +28,7 @@ public class Winner {
         
     }
 
-    public bolean match( int rowG, int colG) // check if hit
+    public boolean match( int rowG, int colG) // check if hit
     {
         tryNum++;
         
@@ -42,37 +44,104 @@ public class Winner {
             System.out.println("Final shot!");
         else
             System.out.println("Guess #" + tryNum);
-    
-        if (answers.grid[rowG][colG] != 'O')
-        {
-            if (sunk())
-                System.out.println("Sunk!");
-            else
-            { 
-                if (answers.grid[rowG][colG] == '*')
-                    guesses.grid[rowG][colG] == 'x';
-                else if (answers.grid[rowG][colG] == 'v' || answers.grid[rowG][colG] == 'v')
-                    guesses.grid[rowG][colG] == '';
-            }
 
+        if (guesses.grid[rowG][colG] != 'O')
+        {
+            System.out.println("You already guessed that spot! Try again. ");
+            tryNum--;
+            guesses.printBoard();
+            return false;
         }
 
+        if (answers.grid[rowG][colG] != 'O')
+        {
+            if (answers.grid[rowG][colG] == '*')
+                guesses.grid[rowG][colG] = '!';
+            else if (answers.grid[rowG][colG] == 'v' || answers.grid[rowG][colG] == '>')
+                guesses.grid[rowG][colG] = '/'; 
+            else
+                guesses.grid[rowG][colG] = 92; // backslash has value 92 and i'm not dealing w/ the headache of escape codes
+            
+            if (sunk(rowG,colG) == 0)
+                System.out.println("Sunk!");
+            else if (sunk(rowG,colG) == 2)
+            {
+                System.out.println("Player 2 wins!");
+                return true;
+            }
+            else
+            {
+                System.out.println("Hit!");
+            }
+
+        }// close if landed somewhere important loop
+        else
+        {
+            guesses.grid[rowG][colG] = '.';
+            System.out.println("Miss!");
+        }
         guesses.printBoard();
         return false;
     }
 
-    public boolean sunk() // check if sunk.. all ships must be sunk for player 1 to win so check here to lessen running time
+    public int sunk(int rowS, colS) // check if sunk.. all ships must be sunk for player 1 to win so check here to lessen running time. return 0 for sunk, 1 for false and 2 for winner
     {
-        int cursor = 0;
-
-
+        int cursor; //cursor should be at left top poitn (\) then continue to check down to  /
         
         if (chickendinner())
         {
             System.out.println("Player 2 wins!");
-            return true;
+            return 2;
         }
         else
-            return false;
+        {
+            if (reOrient)
+            {
+                cursor = colS;//remember for horizontal, the columns are the ones changing
+                while (answers.grid[rowS][cursor] != '<') //scroll all the way to the start
+                    cursor--;
+
+                while (cursor < colNums)
+                {
+                    if (guesses.grid[rowS][cursor] == '/')
+                        return 0;
+                    else if (guess.grid[rowS][cursor] == 'O')
+                        return 1;
+                    cursor++;
+                }
+            }
+            else
+            {
+                cursor = rowS;//remember for horizontal, the columns are the ones changing
+                while (answers.grid[cursor][colS] != '^') //scroll all the way to the start
+                    cursor--;
+
+                while (cursor < rowNums)
+                {
+                    if (guesses.grid[cursor][colS] == '/')
+                        return 0;
+                    else if (guess.grid[cursor][colS] == 'O')
+                        return 1;
+                    cursor++;
+                }
+            }
+        }
+        return 1;
+    }
+
+    public boolean reOrient (rowO, colO )//return true for horizontal, false for vertical
+    {
+        int mousey = colO;
+
+        while (mousey >= 0)
+        {
+            if (answers.grid[rowO][colO] == '<') //check the answers since it's clearer anyways. 
+                return true;
+            else if (answers.grid[rowO][colO] == 'O') //there is defnitely no ship ina blank space
+                return false;
+            mousey--;
+        }
+
+        return false;
     }
 }
