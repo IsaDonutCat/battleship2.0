89c036096c8cc451c0a5143b6e0e1e1a20860537
Isabella Wang <wangisa26@7hills.org>
2023-10-19 15:14:01 -0400

    ship comments

--- Ship.java
+++ Ship.java
@@ -1,18 +1,18 @@
 public class Ship {
     String name;
     int length;
-    public Ship (int size, String called)
+    public Ship (int size, String called) //this is the closest i could get to struct
     {
         name = called;
         length = size;
     }
 
-    public int getSize ()
+    public int getSize () //this doesn't actually have to be changed. so.
     {
         return length;
     }
 
-    public String getName ()
+    public String getName () //getters instead
     {
         return name;
     }
